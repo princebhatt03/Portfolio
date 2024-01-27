6ff0b2c51a8b332bcdebf6766ce690e8e9a4d174
Prince Bhatt <prince25io044@satiengg.in>
2024-01-27 21:28:31 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,69 @@
+var tl = gsap.timeline();
+
+tl.from('h2 ', {
+  y: -30,
+  duration: 0.5,
+  delay: 0.3,
+  stagger: 0.3,
+  opacity: 0,
+});
+tl.from('h1 ', {
+  y: -30,
+  duration: 0.5,
+  delay: 0.3,
+  stagger: 0.3,
+  opacity: 0,
+});
+tl.from('p', {
+  y: -30,
+  duration: 0.5,
+  delay: 0.3,
+  stagger: 0.3,
+  opacity: 0,
+});
+tl.from('h4 ', {
+  y: -30,
+  duration: 0.5,
+  delay: 0.3,
+  stagger: 0.3,
+  opacity: 0,
+});
+tl.from('h6 ', {
+  y: -30,
+  duration: 0.8,
+  delay: 0.3,
+  stagger: 0.3,
+  opacity: 0,
+});
+tl.from('h3', {
+  x: -30,
+  duration: 0.8,
+  delay: 0.3,
+  stagger: 0.3,
+  opacity: 0,
+});
+tl.from('h5', {
+  x: -30,
+  duration: 0.8,
+  delay: 0.3,
+  stagger: 0.3,
+  opacity: 0,
+});
+
+const hamburger = document.querySelector('.hamMenu');
+const navMenu = document.querySelector('.navbar ul');
+
+hamburger.addEventListener('click', mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+}
+const navLink = document.querySelectorAll('.navbar a');
+
+navLink.forEach(n => n.addEventListener('click', closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+}
